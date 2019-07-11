94ae34ff60c7684bd5efdc91245717735a226be0
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-11 16:18:35 -0700

    song section

--- shows.js
+++ shows.js
@@ -1,3 +1,15 @@
 function showsMobile() {
-  var table = document.getElementsByClassName("shows");
+  var table = document.createElement("table");
+
+  var date = document.createElement("td");
+  date.classList.add("shows__date");
+
+  var venue = document.createElement("td");
+  venue.classList.add("shows__date");
+
+  var tr = document.createElement("tr");
+  tr.appendChild(date);
+  tr.appendChild(venue);
+
+  table.appendChild(tr);
 }
