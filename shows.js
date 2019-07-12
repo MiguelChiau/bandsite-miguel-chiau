fa7015bdce148b708f10af35b064802698f5d904
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-12 16:07:02 -0700

    comment basic layout table

--- shows.js
+++ shows.js
@@ -1,15 +1,24 @@
-function showsMobile() {
-  var table = document.createElement("table");
+// This array will include 3 objects with the defaults comments
 
-  var date = document.createElement("td");
-  date.classList.add("shows__date");
+var defaultComments = [
+  {
+    name: "Micheal Lyons",
+    timestamp: "12/18/2018",
+    comment:
+      "They BLEW the ROOF off at their last show, once overyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
+  },
 
-  var venue = document.createElement("td");
-  venue.classList.add("shows__date");
+  {
+    name: "Gary Wong",
+    timestamp: "12/12/2018",
+    comment:
+      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!"
+  },
 
-  var tr = document.createElement("tr");
-  tr.appendChild(date);
-  tr.appendChild(venue);
-
-  table.appendChild(tr);
-}
+  {
+    name: "Theodore Duncan",
+    timestamp: "12/15/2018",
+    comment:
+      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitly my favorite ever!"
+  }
+];
