136f888feee55148ed4529fc2c2f123663b164e4
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-18 10:00:42 -0700

    GET request comments and GET request showDates from API

--- sprint-3/shows.js
+++ sprint-3/shows.js
@@ -1,112 +1,112 @@
-// // This array will include 3 objects with the defaults comments
+// This array will include 3 objects with the defaults comments
 
-// var defaultComments = [
-//   {
-//     name: "Micheal Lyons",
-//     timestamp: "12/18/2018",
-//     comment:
-//       "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
-//   },
+/*var defaultComments = [
+  {
+    name: "Micheal Lyons",
+    timestamp: "12/18/2018",
+    comment:
+      "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
+  },
 
-//   {
-//     name: "Gary Wong",
-//     timestamp: "12/12/2018",
-//     comment:
-//       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!"
-//   },
+  {
+    name: "Gary Wong",
+    timestamp: "12/12/2018",
+    comment:
+      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!"
+  },
 
-//   {
-//     name: "Theodore Duncan",
-//     timestamp: "12/15/2018",
-//     comment:
-//       "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitly my favorite ever!"
-//   }
-// ];
+  {
+    name: "Theodore Duncan",
+    timestamp: "12/15/2018",
+    comment:
+      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitly my favorite ever!"
+  }
+];
 
-// function displayComment(commentObject) {
-//   console.log(commentObject);
-//   //we select our HTML section/ div by it's id
-//   var commentSection = document.querySelector("#originalComments");
-//   // document.getElementById("#originalComments").reset();
-//   var outerDiv = document.createElement("div");
+function displayComment(commentObject) {
+  console.log(commentObject);
+  //we select our HTML section/ div by it's id
+  var commentSection = document.querySelector("#originalComments");
+  // document.getElementById("#originalComments").reset();
+  var outerDiv = document.createElement("div");
 
-//   //now create a div for the profile icon
-//   var profileDiv = document.createElement("div");
-//   outerDiv.appendChild(profileDiv);
-//   commentSection.prepend(outerDiv);
-//   profileDiv.setAttribute("class", "profile");
+  //now create a div for the profile icon
+  var profileDiv = document.createElement("div");
+  outerDiv.appendChild(profileDiv);
+  commentSection.prepend(outerDiv);
+  profileDiv.setAttribute("class", "profile");
 
-//   //create an empty div to hold name+date and comment
-//   var nameDateCommentDiv = document.createElement("div");
-//   outerDiv.appendChild(nameDateCommentDiv);
-//   commentSection.prepend(outerDiv);
-//   nameDateCommentDiv.setAttribute("class", "nameDateComment");
+  //create an empty div to hold name+date and comment
+  var nameDateCommentDiv = document.createElement("div");
+  outerDiv.appendChild(nameDateCommentDiv);
+  commentSection.prepend(outerDiv);
+  nameDateCommentDiv.setAttribute("class", "nameDateComment");
 
-//   //create an empty div to hold name+date
-//   var nameDateDiv = document.createElement("div");
-//   nameDateCommentDiv.appendChild(nameDateDiv);
-//   commentSection.prepend(outerDiv);
-//   // nameDateDiv.setAttribute("class", "nameDate");
-//   nameDateDiv.classList.add("nameDate");
+  //create an empty div to hold name+date
+  var nameDateDiv = document.createElement("div");
+  nameDateCommentDiv.appendChild(nameDateDiv);
+  commentSection.prepend(outerDiv);
+  // nameDateDiv.setAttribute("class", "nameDate");
+  nameDateDiv.classList.add("nameDate");
 
-//   // div for the name
-//   var nameDiv = document.createElement("div");
-//   nameDiv.innerText = commentObject.name;
-//   nameDateDiv.appendChild(nameDiv);
-//   commentSection.prepend(outerDiv);
-//   nameDiv.classList.add("name");
+  // div for the name
+  var nameDiv = document.createElement("div");
+  nameDiv.innerText = commentObject.name;
+  nameDateDiv.appendChild(nameDiv);
+  commentSection.prepend(outerDiv);
+  nameDiv.classList.add("name");
 
-//   // div for the date
-//   var dateDiv = document.createElement("div");
-//   dateDiv.innerText = commentObject.timestamp;
-//   nameDateDiv.appendChild(dateDiv);
-//   commentSection.prepend(outerDiv);
-//   dateDiv.classList.add("date");
+  // div for the date
+  var dateDiv = document.createElement("div");
+  dateDiv.innerText = commentObject.timestamp;
+  nameDateDiv.appendChild(dateDiv);
+  commentSection.prepend(outerDiv);
+  dateDiv.classList.add("date");
 
-//   //for the actual comment
-//   var commentDiv = document.createElement("div");
-//   commentDiv.innerText = commentObject.comment;
-//   nameDateCommentDiv.appendChild(commentDiv);
-//   commentSection.appendChild(outerDiv);
+  //for the actual comment
+  var commentDiv = document.createElement("div");
+  commentDiv.innerText = commentObject.comment;
+  nameDateCommentDiv.appendChild(commentDiv);
+  commentSection.appendChild(outerDiv);
 
-//   // For test purposes
-//   outerDiv.classList.add("mainDiv");
-// }
+  // For test purposes
+  outerDiv.classList.add("mainDiv");
+}
 
-// // Get the form on HTML using DOM
-// document.querySelector("form").addEventListener("submit", function(event) {
-//   // This is to prevent the browser from reloading
-//   event.preventDefault();
+// Get the form on HTML using DOM
+document.querySelector("form").addEventListener("submit", function(event) {
+  // This is to prevent the browser from reloading
+  event.preventDefault();
 
-//   //Now to get the info that the user types in the form fields
-//   const name = event.target.name.value;
-//   const comment = event.target.comment.value;
-//   // const timestamp = event.target.timestamp.value;
+  //Now to get the info that the user types in the form fields
+  const name = event.target.name.value;
+  const comment = event.target.comment.value;
+  // const timestamp = event.target.timestamp.value;
 
-//   console.log(name, comment);
+  console.log(name, comment);
 
-//   //now we want to push this info into an array
-//   //Since we want new comments on top then I'll use unshift intead of push
-//   defaultComments.unshift({
-//     name: name,
-//     comment: comment
-//     // timestamp: timestamp
-//   });
+  //now we want to push this info into an array
+  //Since we want new comments on top then I'll use unshift intead of push
+  defaultComments.unshift({
+    name: name,
+    comment: comment
+    // timestamp: timestamp
+  });
 
-//   displayComments();
-//   event.target.name.value = "";
-//   event.target.comment.value = "";
-//   console.log(defaultComments);
-// });
+  displayComments();
+  event.target.name.value = "";
+  event.target.comment.value = "";
+  console.log(defaultComments);
+});
 
-// function displayComments() {
-//   console.log(defaultComments);
-//   document.querySelector("#originalComments").innerHTML = "";
-//   for (var i = 0; i < defaultComments.length; i++) {
-//     displayComment(defaultComments[i]);
-//   }
-// }
-// displayComments();
+function displayComments() {
+  console.log(defaultComments);
+  document.querySelector("#originalComments").innerHTML = "";
+  for (var i = 0; i < defaultComments.length; i++) {
+    displayComment(defaultComments[i]);
+  }
+}
+displayComments();*/
 
 // Below is the URL for the API
 const URL = "https://project-1-api.herokuapp.com/";
@@ -117,11 +117,22 @@ var myKey = "3b8180e7-1bb1-4f96-8b58-201c916d970b";
 //Then I append my API key into the URL
 
 const fullURL =
-  "https://project-1-api.herokuapp.com/?api_key=3b8180e7-1bb1-4f96-8b58-201c916d970b";
+  "https://project-1-api.herokuapp.com/comments?api_key=3b8180e7-1bb1-4f96-8b58-201c916d970b";
 
-//Sending a request to get a list of comments
+//Sending a request to GET comments, note that "fullURL" includes /comments
 const promise = axios.get(fullURL);
 promise.then(function(response) {
   const data = response.data;
   console.log(data);
+});
+
+//Sending a request to GET showdates
+
+const showsURL =
+  "https://project-1-api.herokuapp.com/showdates?api_key=3b8180e7-1bb1-4f96-8b58-201c916d970b";
+
+const shows = axios.get(showsURL);
+shows.then(function(response) {
+  const showDates = response.data;
+  console.log(showDates);
 });
