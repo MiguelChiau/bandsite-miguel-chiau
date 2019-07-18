2e492359e2180429ea2a386445ab90e6d8ec16b1
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-17 20:39:13 -0700

    fixed issue with paths

--- sprint-2/shows.js
+++ sprint-2/shows.js
@@ -5,7 +5,7 @@ var defaultComments = [
     name: "Micheal Lyons",
     timestamp: "12/18/2018",
     comment:
-      "They BLEW the ROOF off at their last show, once overyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
+      "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed"
   },
 
   {
