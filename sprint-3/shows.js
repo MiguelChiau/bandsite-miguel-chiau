5cc728590dc85534336431c29d92fd02b693614a
Miguel Chiau <chiau.miguel@gmail.com>
2019-07-19 15:03:42 -0700

    Get request for showdates

--- sprint-3/shows.js
+++ sprint-3/shows.js
@@ -5,6 +5,23 @@ const showsURL = `https://project-1-api.herokuapp.com/showdates?api_key=${myKey}
 
 const showsPromise = axios.get(showsURL);
 showsPromise.then(response => {
-  // displayAllComments(response.data);
-  console.log(response.data);
+  allShows(response.data);
 });
+
+// This will loop through the showdates array
+function allShows(shows) {
+  for (let i = 0; i < shows.length; i++) {
+    console.log(shows[i]);
+    document.querySelector("tbody").innerHTML += `<tr class="divider">
+                <td data-label="Date">${shows[i].date}</td>
+                <td data-label="Venue" class="show__venue">${
+                  shows[i].place
+                }</td>
+                <td data-label="Location" class="show__location">
+                  ${shows[i].location}
+                </td>
+  
+                <td><button type="button">BUY TICKETS</button></td>
+              </tr> `;
+  }
+}
